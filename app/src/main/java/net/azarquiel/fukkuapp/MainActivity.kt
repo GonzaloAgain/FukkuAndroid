a345e6a7e63a20f23445ed651430c9447c8a44ff
jonay <alumno.102887@ies-azarquiel.es>
2019-04-18 19:10:12 +0200

    primer dato en firebase

--- app/src/main/java/net/azarquiel/fukkuapp/MainActivity.kt
+++ app/src/main/java/net/azarquiel/fukkuapp/MainActivity.kt
@@ -10,6 +10,10 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +34,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("pepe")
     }
 
     override fun onBackPressed() {
