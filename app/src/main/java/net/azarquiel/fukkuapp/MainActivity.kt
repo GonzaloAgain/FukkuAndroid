7f4ddc84b6cee415c7942b76a544f4c7c69f435b
Gonzalo <alumno.116533@ies-azarquiel.es>
2019-04-18 19:33:16 +0200

    Pruebas iniciales

--- app/src/main/java/net/azarquiel/fukkuapp/MainActivity.kt
+++ app/src/main/java/net/azarquiel/fukkuapp/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
-        myRef.setValue("hola")
+        myRef.setValue("hola desde gonzalo develop")
     }
 
     override fun onBackPressed() {
